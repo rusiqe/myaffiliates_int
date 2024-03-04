193963ce6dcc1a7e603dd2e4dc7218d088b104fe
rusiqe <rusiqe@gmail.com>
2024-03-04 15:07:29 +0100

    remove errors

--- logged.py
+++ logged.py
@@ -21,7 +21,7 @@ def search_for_urls_in_wordpress(sites_directory, url_pattern):
     for site in os.listdir(sites_directory):
         site_path = os.path.join(sites_directory, site)
         if os.path.isdir(site_path):
-          wp_config_path = os.path.join(site_path, 'wp-config.php')
+            wp_config_path = os.path.join(site_path, 'wp-config.php')
             if os.path.isfile(wp_config_path):
                 db_config = extract_db_config(wp_config_path)
                 if db_config:
@@ -50,12 +50,10 @@ def search_for_urls_in_wordpress(sites_directory, url_pattern):
                     except pymysql.Error as e:
                         logger.error("Error searching for URLs for site %s", site)
                         logger.exception(e)
-                    else:
-                        logger.error("Unknown error searching for URLs for site %s", site)
-           finally:
-                logger.info("Finished processing site %s", site)
+                   
     logger.info("Finished processing all sites")
-    
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         print("Usage: python find_links.py <sites_directory> <url_pattern>")
